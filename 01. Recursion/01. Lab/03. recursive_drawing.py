6927c58982e0f62188c968818ac6a0c1b8e5ff4c
dandr94 <d.andreev1994@gmail.com>
2023-01-07 18:25:31 +0200

    Added lab 03. for Recursion

---
+++ 01. Recursion/01. Lab/03. recursive_drawing.py
@@ -0,0 +1,20 @@
+"""
+Write a program that draws the figure below depending on n.
+"""
+
+
+def draw(n: int) -> None:
+    if n == 0:
+        return
+
+    print('*' * n)
+
+    draw(n - 1)
+
+    print('#' * n)
+
+
+draw(5)
+
+# Test solution at:
+# https://judge.softuni.org/Contests/Practice/Index/3459#2
