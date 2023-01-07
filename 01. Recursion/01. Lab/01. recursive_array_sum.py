8430f21e19b9489ea2692dff784b46f8d375cc02
dandr94 <d.andreev1994@gmail.com>
2023-01-07 17:39:48 +0200

    Added lab 01. for Recursion

---
+++ 01. Recursion/01. Lab/01. recursive_array_sum.py
@@ -0,0 +1,38 @@
+"""
+Write a program that finds the sum of all elements in an integer array.
+Use recursion.
+"""
+
+
+def calculate(nums, index):
+    current_element = nums[index]
+    last_element = len(nums) - 1
+
+    if index == last_element:  # Bottom for when recursion should stop
+        return current_element
+
+    next_element = index + 1
+
+    return current_element + calculate(nums, next_element)
+
+
+"""
+1. Taking input as string with spaces;
+Example - 1 2 3 4
+2. Converting the input into list with .split() function;
+3. Traversing the list and converting every str number into int.
+"""
+nums_input = [int(num) for num in input().split()]
+
+# Asserting the result before printing it
+expected_result = 10
+
+result = calculate(nums_input, 0)
+
+assert result == expected_result
+
+# If all good prints the result
+print(result)
+
+# Test solution at:
+# https://judge.softuni.org/Contests/Practice/Index/3459#0
