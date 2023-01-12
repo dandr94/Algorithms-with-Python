e952c76f0cf04fb8c93e314018ee194049ed53ef
dandr94 <d.andreev1994@gmail.com>
2023-01-12 20:02:52 +0200

    Added lab 03. for Searching-and-Sorting-Algorithms

---
+++ 02. Searching-and-Sorting-Algorithms/01. Lab/03. bubble_sort.py
@@ -0,0 +1,22 @@
+"""
+Write an implementation of Bubble Sort.
+You should read an array of integers and sort them.
+"""
+
+from typing import List
+
+
+def bubble_sort(nums: List[int]) -> List[int]:
+    for i in range(len(nums)):
+        for j in range(1, len(nums) - i):
+            if nums[j] < nums[j - 1]:
+                nums[j], nums[j - 1] = nums[j - 1], nums[j]
+
+    return nums
+
+
+numbers = [int(x) for x in input().split()]
+print(*bubble_sort(numbers), sep=' ')
+
+# Test solution at:
+# https://judge.softuni.org/Contests/Practice/Index/3461#2
