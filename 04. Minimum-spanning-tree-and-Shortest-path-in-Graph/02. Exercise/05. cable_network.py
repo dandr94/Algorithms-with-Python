869c6ffd35dd16f220a78723f62b25ec057eac63
dandr94 <d.andreev1994@gmail.com>
2023-01-24 12:17:49 +0200

    Added blank

---
+++ 04. Minimum-spanning-tree-and-Shortest-path-in-Graph/02. Exercise/05. cable_network.py
@@ -0,0 +1,12 @@
+"""
+A cable networking company plans to extend its existing cable network by connecting as many customers as possible within a fixed budget limit.
+The company has calculated the cost of building some prospective connections.
+You are given the existing cable network (a set of customers and connections between them) along with the estimated connection costs between some pairs of customers and prospective customers.
+A customer can only be connected to the network via a direct connection with an already connected customer.
+
+"""
+
+
+
+# Test solution at:
+# https://judge.softuni.org/Contests/Practice/Index/3465#4
