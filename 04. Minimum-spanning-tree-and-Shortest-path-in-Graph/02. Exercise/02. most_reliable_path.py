509fba9b10ffdfc9d6233db3d1d3b0185c51fe09
dandr94 <d.andreev1994@gmail.com>
2023-01-24 12:17:21 +0200

    Added blank

---
+++ 04. Minimum-spanning-tree-and-Shortest-path-in-Graph/02. Exercise/02. most_reliable_path.py
@@ -0,0 +1,11 @@
+"""
+We have a set of towns and some of them are connected by direct paths.
+Each path has a coefficient of reliability (in percentage): the chance to pass without incidents.
+Your goal is to compute the most reliable path between two given nodes.
+Assume all percentages will be integer numbers and round the result to the second digit after the decimal separator.
+"""
+
+
+
+# Test solution at:
+# https://judge.softuni.org/Contests/Practice/Index/3465#1
