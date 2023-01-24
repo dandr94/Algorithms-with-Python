2ed4273dc95ec71b0ea2d972f8939a63b03c2658
dandr94 <d.andreev1994@gmail.com>
2023-01-24 12:17:41 +0200

    Added blank

---
+++ 04. Minimum-spanning-tree-and-Shortest-path-in-Graph/02. Exercise/04. undefined.py
@@ -0,0 +1,8 @@
+"""
+Your task is to find the shortest path in a graph from S vertex to D vertex. However, edges might have negative weights and for this reason, you should be cautious for negative cycles.
+"""
+
+
+
+# Test solution at:
+# https://judge.softuni.org/Contests/Practice/Index/3465#3
