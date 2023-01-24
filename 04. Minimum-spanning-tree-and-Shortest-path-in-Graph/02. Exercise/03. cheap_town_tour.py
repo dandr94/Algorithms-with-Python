63bbbbec23899c6417d886415020e924feab5864
dandr94 <d.andreev1994@gmail.com>
2023-01-24 12:17:31 +0200

    Added blank

---
+++ 04. Minimum-spanning-tree-and-Shortest-path-in-Graph/02. Exercise/03. cheap_town_tour.py
@@ -0,0 +1,11 @@
+"""
+You now live in a new country, and you want to start a tour and visit every town in the country and since you are new in the country your finances are minimized, so you want your trip to be as cheap as possible.
+You will be given the amount of the cities on the first line, then the amount of the roads (n), and on the next n lines you will receive which tows the road connects and the cost to use it.
+Assume you can start from any town and your target is to visit every one of them with the minimum cost.
+
+"""
+
+
+
+# Test solution at:
+# https://judge.softuni.org/Contests/Practice/Index/3465#2
