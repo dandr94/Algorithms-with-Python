6e17de0004a7e8ac74ccd55c8f2f201e1e29f806
dandr94 <d.andreev1994@gmail.com>
2023-01-25 14:07:24 +0200

    Added exercise 04. for Minimum-spanning-tree-and-Shortest-path-in-Graph

--- 04. Minimum-spanning-tree-and-Shortest-path-in-Graph/02. Exercise/04. undefined.py
+++ 04. Minimum-spanning-tree-and-Shortest-path-in-Graph/02. Exercise/04. undefined.py
@@ -1,8 +1,97 @@
 """
-Your task is to find the shortest path in a graph from S vertex to D vertex. However, edges might have negative weights and for this reason, you should be cautious for negative cycles.
+Your task is to find the shortest path in a graph from S vertex to D vertex.
+However, edges might have negative weights and for this reason, you should be cautious for negative cycles.
 """
+from collections import deque
+from typing import List, Union, Deque
 
 
+class Edge:
+    def __init__(self, source: int, destination: int, weight: int):
+        self.source = source
+        self.destination = destination
+        self.weight = weight
+
+
+def build_graph(edges: int) -> List[Edge]:
+    graph = []
+
+    for _ in range(edges):
+        source, destination, weight = [int(x) for x in input().split()]
+
+        graph.append(Edge(source, destination, weight))
+
+    return graph
+
+
+def bfs(graph: List[Edge], start: int, end: int) -> None:
+    distance = [float('inf')] * (nodes + 1)
+    distance[start] = 0
+    parent = [None] * (nodes + 1)
+
+    for _ in range(nodes - 1):
+        updated = False
+        for edge in graph:
+            if distance[edge.source] == float('inf'):
+                continue
+
+            new_distance = distance[edge.source] + edge.weight
+
+            if new_distance < distance[edge.destination]:
+                distance[edge.destination] = new_distance
+                parent[edge.destination] = edge.source
+                updated = True
+
+        if not updated:
+            break
+
+    return print_results(graph, parent, distance, end)
+
+
+def is_cycle(graph: List[Edge], distance: List[Union[float, int]]) -> bool:
+    for edge in graph:
+        new_distance = distance[edge.source] + edge.weight
+
+        if new_distance < distance[edge.destination]:
+            return True
+
+    return False
+
+
+def reconstruct_path(parent: List[Union[None, int]], end: int) -> Deque[int]:
+    path = deque()
+    node = end
+
+    while node is not None:
+        path.appendleft(node)
+        node = parent[node]
+
+    return path
+
+
+def print_results(graph: List[Edge],
+                  parent: List[Union[None, int]],
+                  distance: List[Union[float, int]],
+                  end: int) -> None:
+
+    if is_cycle(graph, distance):
+        print('Undefined')
+    else:
+        path = reconstruct_path(parent, end)
+
+        print(*path, sep=' ')
+        print(distance[end])
+
+
+nodes = int(input())
+edges = int(input())
+
+graph = build_graph(edges)
+
+start = int(input())
+end = int(input())
+
+bfs(graph, start, end)
 
 # Test solution at:
 # https://judge.softuni.org/Contests/Practice/Index/3465#3
